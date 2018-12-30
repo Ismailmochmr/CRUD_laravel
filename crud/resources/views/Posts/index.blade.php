d60d7ba44bdf1d58636edc7bbd561c130b0f7a3f
ismailmochmr <1157050080@student.uinsgd.ac.id>
2018-12-30 22:03:24 +0700

    latihcrud

---
+++ crud/resources/views/Posts/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+@section('content')
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="full-right">
+				<h2>CRUD</h2>
+			</div>
+		</div>
+	</div>
+@endsection
